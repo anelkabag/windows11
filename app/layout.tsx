ada4dd2cc7b3073cd0424f875df152951322a2c4
Anelka MD <anelkabagalaneofficiel@gmail.com>
2025-05-04 19:16:41 +0200

    update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,8 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Windows 11 - Clone',
+  description: 'Created with Anelka MD',
 }
 
 export default function RootLayout({
